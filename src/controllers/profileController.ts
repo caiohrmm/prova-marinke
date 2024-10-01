353021e1e127a421af142b2b1238988be0bd2f45
FelipeViol <violluisfelipe@gmail.com>
2024-09-30 21:19:24 -0300

    Criando endpoints de lógica

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -1,33 +1,39 @@
 import { Request, Response } from 'express';
 import Profile from '../models/Profile';
+import { sequelize } from '../config/db'
 
-// Criar um novo perfil
-export const createProfile = async (req: Request, res: Response) => {
+export const createProfile = async (req: Request, res: Response): Promise<void> => {
+  const transaction = await sequelize.transaction();
+
   try {
     const { firstname, lastname, profession, balance, type } = req.body;
-    const profile = await Profile.create({ firstname, lastname, profession, balance, type });
+
+    if (balance < 0) {
+      res.status(400).json({ message: 'Saldo não pode ser negativo!' });
+      return;
+    }
+
+    // Criar o perfil dentro da transação
+    const profile = await Profile.create({ firstname, lastname, profession, balance, type }, { transaction });
+
+    // Confirmar a transação
+    await transaction.commit();
+
     res.status(201).json(profile);
   } catch (error) {
+    await transaction.rollback();
+    console.error('Erro ao criar perfil:', error);
     res.status(500).json({ error: 'Erro ao criar perfil.' });
   }
 };
 
-// Obter todos os perfis
-export const getProfiles = async (req: Request, res: Response) => {
-  try {
-    const profiles = await Profile.findAll();
-    res.status(200).json(profiles);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao buscar perfis.' });
-  }
-};
 
 // Obter um perfil pelo ID
-export const getProfileById = async (req: Request, res: Response) => {
+export const verifyBalanceById = async (req: Request, res: Response) => {
   try {
     const profile = await Profile.findByPk(req.params.id);
     if (profile) {
-      res.status(200).json(profile);
+      res.status(200).json(profile.balance);
     } else {
       res.status(404).json({ error: 'Perfil não encontrado.' });
     }
